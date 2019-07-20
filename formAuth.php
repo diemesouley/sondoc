91b52e9e7ba886b611729aaf0e73bc921e763fae
Souleymane <diemesouley@gmail.com>
2019-07-20 20:38:37 +0000

    ptojet init

---
+++ formAuth.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>Accueil</title>
+</head>
+<body>
+<div id="container">
+        <form action="" method="POST">
+        <h1>Connexion</h1>
+      
+        <input type="text" placeholder="Entrer le nom d'utilisateur" name="Login" required>
+     
+         <input type="password" placeholder="Entrer le mot de passe" name="Password" required>
+        <input type="submit" id='submit' value='CONNEXION' name="CONNEXION" >              
+        </form>
+  </div>
+
+</body>
+</html>
